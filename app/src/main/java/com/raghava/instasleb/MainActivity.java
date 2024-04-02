024531b8b3533ed66eedde372d3240800265ce87
raghava7129 <21cs01068@iitbbs.ac.in>
2024-04-02 17:31:14 +0530

    Implements main screen activity.

--- app/src/main/java/com/raghava/instasleb/MainActivity.java
+++ app/src/main/java/com/raghava/instasleb/MainActivity.java
@@ -1,14 +1,46 @@
 package com.raghava.instasleb;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    AppCompatButton yes_btn, no_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().hide();
+        init_views();
+
+        yes_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, homePage.class);
+                startActivity(intent);
+                overridePendingTransition(0, 0);
+            }
+        });
+
+        no_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finishAffinity();
+                overridePendingTransition(0, 0);
+            }
+        });
+    }
+
+    private void init_views() {
+        yes_btn = findViewById(R.id.yes_btn);
+        no_btn = findViewById(R.id.no_btn);
+
     }
 }
