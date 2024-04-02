4d73f8a5eeead35e9fd3742ca58470a29ace332b
raghava7129 <21cs01068@iitbbs.ac.in>
2024-04-02 09:36:41 +0530

    Initializes project

---
+++ app/src/main/java/com/raghava/instasleb/MainActivity.java
@@ -0,0 +1,14 @@
+package com.raghava.instasleb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
